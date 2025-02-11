062e14b63daeae5b9d9e224dece828183d242c4c
sahanDlenora <sdlenora@gmail.com>
2025-02-12 01:21:54 +0530

    update

--- ecommerce_project/api/urls.py
+++ ecommerce_project/api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Registration URL
+    # Url for registration
     path('register/', RegisterView.as_view(), name='register'),
 
     # Router URLs for viewsets (automatic routing)
